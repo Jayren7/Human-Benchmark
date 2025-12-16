5eb2f54563027b22011b0e953419ad4b746c972c
Jayren <jren698@gmail.com>
2025-12-16 13:16:08 +0700

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Human-Benchmark/',   // NAMA REPO
-  build: {
-    outDir: 'docs',            // SESUAI GITHUB PAGES
-  },
   plugins: [react()],
+  base: '/Human-Benchmark/',
+  build: {
+    outDir: 'docs'
+  }
 })
